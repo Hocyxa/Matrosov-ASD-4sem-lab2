de89a474af5606c4c24b01cce6e87474581fd9e8
Максим Матросов <matrosov.02@bk.ru>
2022-05-05 23:47:32 +0400

    Проблема с определнием размера tmv1 и tmv2, прога ломается из=за этого, завтра решить

--- ASD 4.2/Source.cpp
+++ ASD 4.2/Source.cpp
@@ -56,12 +56,123 @@ stats Sort_Of_Shell(std::vector <int>& v)
 
 }
 
+bool sorted(std::vector<int>& data)
+{
+	for (size_t i = 0; i < data.size()-1; i++)
+	{
+	
+		if (data[i] >= data[i+1]) return false;
+	
+	}
+	return true;
+}
 
+std::vector<int> Merge(std::vector<int>& v1, std::vector<int>& v2, size_t size)
+{
+	std::vector<int> ret (size); //возращаемый массив
+	size_t i = 0; //индекс первого массива
+	size_t j = 0; // индекс второго массива
+	size_t k = 0; //индекс возращаемого массива
+	while (k != size)
+	{
+		if (i == v1.size())
+		{
+			ret[k] = v2[j];
+			j++;
+			k++;
+			continue;
+		}
+		if (j == v2.size())
+		{
+			ret[k] = v1[i];
+			i++;
+			k++;
+			continue;
+		}
+		if (v1[i] < v2[j])
+		{
+			ret[k] = v1[i];
+			i++;
+		}
+		else
+		{
+			ret[k] = v2[j];
+			j++;
+		}
+		k++;
+	}
+	return ret;
+}
+
+stats Two_Way_Merge_Sort(std::vector<int>& data) 
+{
+	stats stat;
+	std::vector<int> tmv(data.size());
+
+	while (1)
+	{
+		std::vector<int> tmv1(data.size());
+		std::vector<int> tmv2(data.size());
+		int count = 1; //проверка чётности итерации
+		size_t l_old = 0; //пиндекс левой стороны data
+		size_t r_old = tmv.size() - 1; //индекс правой стороны data
+		size_t l_new = 0; //пиндекс левой стороны tmv
+		size_t r_new = tmv.size() - 1; //индекс правой стороны tmv
+		while (l_old != r_old)
+		{
+			size_t left_size = 0;//количество элементов слева
+			size_t right_size = 0;//количесвто элементов справа
+			tmv1[0] = data[l_old];
+			for (size_t i = 0; (data[l_old] <= data[l_old + 1]); l_old++)
+			{
+				tmv1[i] = data[l_old];
+				if (l_old == tmv.size() - 1) return stat; // <---------------------------------------< return
+				i++;
+				left_size++;
+			}
+			if (left_size == 0) left_size++;
+			tmv2[0] = data[r_old ];
+			for (size_t i = 0; data[r_old] <= data[r_old - 1]; r_old--)
+			{
+				tmv2[i] = data[r_old];
+				i++;
+				right_size++;
+			}
+			if (right_size == 0) right_size++;
+			std::vector<int> tmv0(left_size + right_size);
+			tmv0 = Merge(tmv1, tmv2, left_size + right_size);
+			if (count % 2 == 1) //пишем слева
+			{
+				for (size_t i = 0; i < left_size + right_size; l_new++)
+				{
+					tmv[l_new] = tmv0[i];
+					i++;
+				}
+				count++;
+			}
+			if (count % 2 == 0) //пишем справа
+			{
+				for (size_t i = 0; i < left_size + right_size; r_new--)
+				{
+					tmv[r_new] = tmv[0];
+					i++;
+				}
+				count++;
+			}
+		}
+
+
+
+
+		
+	}
+
+}
 
 int main()
 {
 	stats stat;
-	
+	/*
 	std::vector<int> v1 = { 8,1,3,10,4,5,2,9,7,6 };
 	for (auto i = v1.begin(); i != v1.end(); ++i)
 	{
@@ -93,15 +204,17 @@ int main()
 	std::cout << "\n";
 	std::cout << "Number of comparisons:" << stat.comparison_count << std::endl;
 	std::cout << "Number of copies:" << stat.copy_count << std::endl;
-	
+	*/
 
-	std::vector<int> v3 = { 8,1,3,10,4,5,2,9,7,6 };
+
+	std::vector<int> v3= { 8,1,3,10,4,5,2,9,7,6 };
 	for (auto i = v3.begin(); i != v3.end(); ++i)
 	{
 		std::cout << *i << " ";
 	}
 	std::cout << "\n";
-//	stat = Heap_Sort(v3);
+	stat = Two_Way_Merge_Sort(v3);
+
 	for (auto i = v3.begin(); i != v3.end(); ++i)
 	{
 		std::cout << *i << " ";

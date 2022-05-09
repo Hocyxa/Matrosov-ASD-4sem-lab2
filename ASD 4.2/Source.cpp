84a940e094509b90120675a0936db9b99616af31
Максим Матросов <matrosov.02@bk.ru>
2022-05-09 15:30:31 +0400

    изменил логику сортировки естественным двухпутевым слиянием, дописал рандомное создание векторов

--- ASD 4.2/Source.cpp
+++ ASD 4.2/Source.cpp
@@ -61,25 +61,26 @@ bool sorted(std::vector<int>& data)
 	for (size_t i = 0; i < data.size()-1; i++)
 	{
 	
-		if (data[i] >= data[i+1]) return false;
+		if (data[i] > data[i+1]) return false;
 	
 	}
 	return true;
 }
 
-std::vector<int> Merge(std::vector<int>& v1, std::vector<int>& v2, size_t size)
+std::vector<int> Merge(std::vector<int>& v1, std::vector<int>& v2, stats & stat)
 {
-	std::vector<int> ret (size); //возращаемый массив
+	std::vector<int> ret (v1.size() + v2.size()); //возращаемый массив
 	size_t i = 0; //индекс первого массива
 	size_t j = 0; // индекс второго массива
 	size_t k = 0; //индекс возращаемого массива
-	while (k != size)
+	while (k != v1.size() + v2.size())
 	{
 		if (i == v1.size())
 		{
 			ret[k] = v2[j];
 			j++;
 			k++;
+			stat.copy_count++;
 			continue;
 		}
 		if (j == v2.size())
@@ -87,6 +88,7 @@ std::vector<int> Merge(std::vector<int>& v1, std::vector<int>& v2, size_t size)
 			ret[k] = v1[i];
 			i++;
 			k++;
+			stat.copy_count++;
 			continue;
 		}
 		if (v1[i] < v2[j])
@@ -98,98 +100,98 @@ std::vector<int> Merge(std::vector<int>& v1, std::vector<int>& v2, size_t size)
 		{
 			ret[k] = v2[j];
 			j++;
+			
 		}
+		stat.copy_count++;
+		stat.comparison_count++;
 		k++;
 	}
 	return ret;
 }
 
-stats Two_Way_Merge_Sort(std::vector<int>& data) 
+stats Naturial_Two_Way_Merge_Sort(std::vector<int>& data, stats stat)
 {
-	stats stat;
+	if (data.size() == 1) return stat;
+	if (sorted(data)) return stat;
 	std::vector<int> tmv(data.size());
-
-	while (1)
+	bool count = true; //проверка чётности итерации
+	size_t l_old = 0; //пиндекс левой стороны data
+	size_t r_old = data.size() - 1; //индекс правой стороны data
+	size_t l_new = 0; //пиндекс левой стороны tmv
+	size_t r_new = data.size() - 1; //индекс правой стороны tmv
+	while (l_old < r_old)
 	{
-
-		bool count = true; //проверка чётности итерации
-		size_t l_old = 0; //пиндекс левой стороны data
-		size_t r_old = tmv.size() - 1; //индекс правой стороны data
-		size_t l_new = 0; //пиндекс левой стороны tmv
-		size_t r_new = tmv.size() - 1; //индекс правой стороны tmv
-		bool flag = false;
-		while (l_old != r_old )
+		std::vector<int> tmv1;
+		std::vector<int> tmv2;
+		while ((data[l_old] <= data[l_old + 1]) && (l_old < r_old) && (l_old < data.size()))
 		{
-		
-			std::vector<int> tmv1;
-			std::vector<int> tmv2;
-			size_t left_size = 0;//количество элементов слева
-			size_t right_size = 0;//количесвто элементов справа
-			for (size_t i = 0; (data[l_old] <= data[l_old + 1]); ++l_old)
-			{
-
-				tmv1.push_back(data[l_old]);
-				if (l_old == tmv.size() - 1) return stat; // <---------------------------------------< return
-				i++;
-				left_size++;
-			}
-			if (data[l_old] > data[l_old + 1])
-			{
-				tmv1.push_back(data[l_old]);
-				left_size++;
-				l_old++;
-			}
-
-			for (size_t i = 0; data[r_old] <= data[r_old - 1]; --r_old)
-			{
-				tmv2.push_back(data[r_old]);
-				i++;
-				right_size++;
-			}
-			if (data[r_old] > data[r_old - 1])
-			{
-				tmv2.push_back(data[r_old]);
-				right_size++;
-				r_old--;
-			}
-
-			std::vector<int> tmv0(left_size + right_size);
-			tmv0 = Merge(tmv1, tmv2, left_size + right_size);
-			if (count == true) //пишем слева
-			{
-				for (size_t i = 0; i < left_size + right_size; l_new++)
-				{
-					tmv[l_new] = tmv0[i];
-					i++;
-				}
-				count = false;
-			}
-			else //пишем справа
-			{
-				for (size_t i = 0; i < left_size + right_size; r_new--)
-				{
-					tmv[r_new] = tmv0[i];
-					i++;
-				}
-				count = true;
-			}
-
-			
+			tmv1.push_back(data[l_old]);
+			l_old++;
+			stat.comparison_count++;
 		}
-		//tmv[r_old] = data[l_old];
-		data = tmv;
-
-
-		
+		tmv1.push_back(data[l_old]);
+		l_old++;
+		while ((data[r_old] <= data[r_old - 1]) && (l_old < r_old) && (r_old >= 0))
+		{
+			tmv2.push_back(data[r_old]);
+			r_old--;
+			stat.comparison_count++;
+		}
+		if (l_old <= r_old)
+		{
+			tmv2.push_back(data[r_old]);
+			r_old--;
+		}
+		std::vector<int> tmv0;
+		tmv0 = Merge(tmv1, tmv2, stat);
+		if (count)//пишем слева
+		{
+			for (size_t i = 0; i < tmv0.size(); l_new++)
+			{
+				tmv[l_new] = tmv0[i];
+				i++;
+				stat.copy_count++;
+			}
+			count = false;
+		}
+		else //пишем справа
+		{
+			for (size_t i = 0; i < tmv0.size(); r_new--)
+			{
+				tmv[r_new] = tmv0[i];
+				i++;
+				stat.copy_count++;
+			}
+			count = true;
+		}
+		if (tmv0.size() == data.size())
+		{
+			data = tmv;
+			stat.copy_count += tmv.size();
+			return stat;
+		}
 	}
-
+	if (l_old != data.size())
+	{
+		stat = Naturial_Two_Way_Merge_Sort(tmv, stat);
+	}
+	data = tmv;
+	stat.copy_count += tmv.size();
+	return stat;
 }
+
+
+
+
 
 int main()
 {
 	stats stat;
-	/*
-	std::vector<int> v1 = { 8,1,3,10,4,5,2,9,7,6 };
+	std::vector<int> v1 (10);
+	for (auto i = v1.begin(); i != v1.end(); ++i)
+	{
+		*i = rand() % 10;
+	}
 	for (auto i = v1.begin(); i != v1.end(); ++i)
 	{
 		std::cout << *i << " ";
@@ -206,7 +208,11 @@ int main()
 
 	std::cout << std::endl;
 
-	std::vector<int> v2 = { 8,1,3,10,4,5,2,9,7,6 };
+	std::vector<int> v2 (10);
+	for (auto i = v2.begin(); i != v2.end(); ++i)
+	{
+		*i = rand() % 10;
+	}
 	for (auto i = v2.begin(); i != v2.end(); ++i)
 	{
 		std::cout << *i << " ";
@@ -220,16 +226,21 @@ int main()
 	std::cout << "\n";
 	std::cout << "Number of comparisons:" << stat.comparison_count << std::endl;
 	std::cout << "Number of copies:" << stat.copy_count << std::endl;
-	*/
+	
+	std::cout << std::endl;
 
-
-	std::vector<int> v3= { 8,1,3,10,4,5,2,9,7,6 };
+	std::vector<int> v3(10);
+	for (auto i = v3.begin(); i != v3.end(); ++i)
+	{
+		*i= rand()%10;
+	}
 	for (auto i = v3.begin(); i != v3.end(); ++i)
 	{
 		std::cout << *i << " ";
 	}
 	std::cout << "\n";
-	stat = Two_Way_Merge_Sort(v3);
+	stats null;
+	stat = Naturial_Two_Way_Merge_Sort(v3, null);
 
 	for (auto i = v3.begin(); i != v3.end(); ++i)
 	{
